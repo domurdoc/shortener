f9405f5476964d8347d168453f0623f59326a2a7
Stanislav Lyan <s.lyan@yandex.ru>
2025-08-10 23:03:36 +0300

    iter1: add Shortener service/repository/handler;

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/domurdoc/shortener/internal/handler"
+	"github.com/domurdoc/shortener/internal/repository"
+	"github.com/domurdoc/shortener/internal/service"
+)
+
+func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+	shortenerHandler := handler.New("http://localhost:8080", service.New(repository.NewMem(), nil))
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /", shortenerHandler.Shorten)
+	mux.HandleFunc("GET /{shortCode}", shortenerHandler.Retrieve)
+	return http.ListenAndServe(":8080", mux)
+}
