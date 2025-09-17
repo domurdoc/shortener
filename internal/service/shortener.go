5be1468d6fb18a6d49904359c7d48f33f5eeaffc
Stanislav Lyan <s.lyan@yandex.ru>
2025-09-17 23:41:27 +0300

    iter12: add Batch operations to Repo, Service, api

--- internal/service/shortener.go
+++ internal/service/shortener.go
@@ -21,21 +21,7 @@ func New(repo repository.Repo, baseURL string) *Shortener {
 }
 
 func (s *Shortener) Shorten(ctx context.Context, longURL string) (string, error) {
-	if len(longURL) > URLMaxLength {
-		return "", &URLError{msg: "url too long", url: longURL}
-	}
-	parsedLongURL, err := url.Parse(longURL)
-	if err != nil {
-		return "", &URLError{msg: err.Error(), url: longURL}
-	}
-	if parsedLongURL.Host == "" {
-		return "", &URLError{msg: "must be absolute", url: longURL}
-	}
-	if parsedLongURL.String() != longURL {
-		return "", &URLError{msg: "must be url-encoded", url: longURL}
-	}
-	shortCode := generateShortCode()
-	shortURL, err := url.JoinPath(s.baseURL, shortCode)
+	shortCode, shortURL, err := s.generateShortCodeURL(longURL)
 	if err != nil {
 		return "", err
 	}
@@ -54,8 +40,56 @@ func (s *Shortener) GetByShortCode(ctx context.Context, shortCode string) (strin
 	return string(url), nil
 }
 
+func (s *Shortener) ShortenBatch(ctx context.Context, longURLS []string) ([]string, error) {
+	shortCodes := make([]string, len(longURLS))
+	shortURLS := make([]string, len(longURLS))
+	batchItems := make([]repository.BatchItem, len(longURLS))
+	for i, longURL := range longURLS {
+		shortCode, shortURL, err := s.generateShortCodeURL(longURL)
+		if err != nil {
+			return nil, err
+		}
+		shortCodes[i] = shortCode
+		shortURLS[i] = shortURL
+		batchItems[i] = repository.BatchItem{Key: repository.Key(shortCode), Value: repository.Value(longURL)}
+	}
+	if err := s.repo.StoreBatch(ctx, batchItems); err != nil {
+		return nil, err
+	}
+	return shortURLS, nil
+}
+
 func (s *Shortener) HealthCheck(ctx context.Context) error {
 	e := make([]error, 0)
 	e = append(e, s.repo.Ping(ctx))
 	return errors.Join(e...)
+}
+
+func (s *Shortener) generateShortCodeURL(originalURL string) (string, string, error) {
+	if err := validateURL(originalURL); err != nil {
+		return "", "", err
+	}
+	shortCode := generateShortCode()
+	shortURL, err := url.JoinPath(s.baseURL, shortCode)
+	if err != nil {
+		return "", "", err
+	}
+	return shortCode, shortURL, nil
+}
+
+func validateURL(URL string) error {
+	if len(URL) > URLMaxLength {
+		return &URLError{msg: "url too long", url: URL}
+	}
+	parsedLongURL, err := url.Parse(URL)
+	if err != nil {
+		return &URLError{msg: err.Error(), url: URL}
+	}
+	if parsedLongURL.Host == "" {
+		return &URLError{msg: "must be absolute", url: URL}
+	}
+	if parsedLongURL.String() != URL {
+		return &URLError{msg: "must be url-encoded", url: URL}
+	}
+	return nil
 }
